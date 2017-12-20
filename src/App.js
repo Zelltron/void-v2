62065336be570e691e413d1c0e46884e10de81ec
Ryan Zell <ryanzell36@gmail.com>
2017-12-19 21:59:39 -0500

    broken detection logic

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import CommandForm from './commandForm';
 
 function typingAnimation(text, timeline) {
   text.each(function() {
-    var mySplitText = new SplitText(this, {type: "words,chars"});
-    var chars = mySplitText.chars;
+    let mySplitText = new SplitText(this, {type: "words,chars"});
+    let chars = mySplitText.chars;
     timeline.staggerFrom(chars, 0.01, {
       opacity: 0,
       ease: Power1.easeIn
@@ -25,8 +25,8 @@ function typingAnimation(text, timeline) {
 
 function questionAnimation(text, timeline) {
   text.each(function() {
-    var mySplitText = new SplitText(this, {type: "words,chars"});
-    var chars = mySplitText.chars;
+    let mySplitText = new SplitText(this, {type: "words,chars"});
+    let chars = mySplitText.chars;
     timeline.staggerFrom(chars, 0.01, {
       opacity: 0,
       ease: Power1.easeIn
@@ -36,15 +36,101 @@ function questionAnimation(text, timeline) {
 
 const tl = new TimelineMax();
 
+/// simple rectangle intersection code, so we can work out what part remains visible
+let intersect = function(r1, r2, bool) {
+    if ( bool ) {
+      return !(r2.left > r1.right ||
+               r2.right < r1.left ||
+               r2.top > r1.bottom ||
+               r2.bottom < r1.top);
+    }
+    else {
+        let r3 = {
+            left: Math.max(r1.left, r2.left),
+            top: Math.max(r1.top, r2.top),
+            right: Math.min(r1.right, r2.right),
+            bottom: Math.min(r1.bottom, r2.bottom)
+        };
+        r3.width = r3.right - r3.left;
+        r3.height = r3.bottom - r3.top;
+        return r3;
+    }
+}
+/// simple function to handle full page scroll, when needed.
+let scrollrect = function(r1){
+    /// update what we know of the page scroll (this affects ClientRects())
+    scrollrect.scrollx = window.pageXOffset || document.documentElement.scrollLeft;
+    scrollrect.scrolly = window.pageYOffset || document.documentElement.scrollTop;
+    /// all because getBoundingClientRect() returns a read-only object (it seems?)
+    return {
+        left: r1.left + scrollrect.scrollx,
+        top: r1.top + scrollrect.scrolly,
+        right: r1.right + scrollrect.scrollx,
+        bottom: r1.bottom + scrollrect.scrolly,
+        width: r1.width,
+        height: r1.height
+    };
+}
+/// add in a jQuery pseudo selector :onscreen, which calculates screen presence
+/// based on getBoundingClientRect() and the full page scroll.
+$.extend(
+  $.expr[':'],
+  {
+    /// check that an element is actually visible on the screen
+    'onscreen': function (el, indx, args) {
+      let $el, ov, r1, r2;
+      r1 = el.getBoundingClientRect();
+      el = el.parentNode;
+      $el = $(el);
+      /// this should loop back all the way to <body>, ignoring <html>
+      do {
+          /// handle different states of overflow
+          ov = $el.css('overflow') || $el.css('overflow-x') + ':' + $el.css('overflow-y');
+          /// special overflow for body
+          if ( $el.is('body') ) { ov = 'body'; }
+          /// if our parent acts as a rectangular mask, intersect the rects
+          switch ( ov ) {
+          case 'hidden':
+          case 'scroll':
+          case 'scroll:hidden':
+          case 'hidden:scroll':
+              r1 = intersect(r1, el.getBoundingClientRect());
+          break;
+          case 'body':
+              r1 = intersect(r1, scrollrect(el.getBoundingClientRect()));
+          break;
+          }
+          if ( r1.width <= 0 || r1.height <= 0 ) {
+              return false;
+          }
+      } while ((el = el.parentNode) && el.parentNode && ($el[0] = el));
+      return true;
+    }
+  }
+);
+
+
 export default class App extends Component {
 
   componentDidMount() {
+    /// this is the elements we are working out the screen visibility of
+    let target = $('.target');
+
+    /// simple update to show result of .filer(':onscreen')
+    let update = function(){
+        let str = '';
+        target.filter(':onscreen').each(function(){
+            str += ($(this).attr('id') + 'is on screen');
+        });
+        console.log(str);
+    };
+
+
     typingAnimation($(".txt"), tl);
     questionAnimation($(".question"), tl);
     tl.to($("#command-form"), 0.5, {opacity: 1});
-    $(window).scroll(function(){
-      $(".tv-container").css("opacity", 1 - $(window).scrollTop() / 250);
-    });
+    $('.scrollable').add(window).scroll(update);
+    update();
   }
 
   render() {
@@ -79,17 +165,26 @@ export default class App extends Component {
                 </div>
               </div>
               <CommandForm {...this.props} timeline={tl}/>
-              <div id="central-body">
+              <div id="central-body" className="scrollable">
                 <div className="tv-row">
-                  <div className="tv-container">
+                  <div id="tv-con-1" className="tv-container target">
                     <img alt="tv-1" className="mainscreen tv" src={process.env.PUBLIC_URL + "/tv.png"}/>
                   </div>
-                  <div className="tv-container">
+                  <div id="tv-con-2" className="tv-container target">
                     <img alt="tv-2" className="mainscreen tv" src={process.env.PUBLIC_URL + "/tv.png"}/>
                     <img alt="dog" id="dog" className="mainscreen tv-contents" src={process.env.PUBLIC_URL + "/dog.gif"}/>
                   </div>
-                  <div className="tv-container">
+                  <div id="tv-con-3" className="tv-container target">
                     <img alt="tv-3" className="mainscreen tv" src={process.env.PUBLIC_URL + "/tv.png"}/>
+                  </div>
+                  <div id="tv-con-4" className="tv-container target">
+                    <img alt="tv-4" className="mainscreen tv" src={process.env.PUBLIC_URL + "/tv.png"}/>
+                  </div>
+                  <div id="tv-con-5" className="tv-container target">
+                    <img alt="tv-5" className="mainscreen tv" src={process.env.PUBLIC_URL + "/tv.png"}/>
+                  </div>
+                  <div id="tv-con-6" className="tv-container target">
+                    <img alt="tv-6" className="mainscreen tv" src={process.env.PUBLIC_URL + "/tv.png"}/>
                   </div>
                 </div>
                 <a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/ryanzell"><img alt="linkedin" className="mainscreen linked" src={process.env.PUBLIC_URL + "/linked.png"}/></a>
