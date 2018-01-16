abba2bc5ec92e259a18659b0c26c2f263b956624
Ryan Zell <rzell@EUCLID2003.local>
2018-01-16 15:58:26 -0500

    change gifs

--- src/components/standardTVs.js
+++ src/components/standardTVs.js
@@ -8,7 +8,7 @@ export default class StandardTVs extends Component {
       <div className="tv-row">
         <TV key="1" imgSrc={process.env.PUBLIC_URL + "/dog.gif"} />
         <TV key="2" imgSrc={process.env.PUBLIC_URL + "/carl.gif"} />
-        <TV key="3" imgSrc={process.env.PUBLIC_URL + "/ghost.gif"} />
+        <TV key="3" imgSrc={process.env.PUBLIC_URL + "/loop.gif"} />
       </div>
     );
   }
